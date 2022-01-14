f24e2c29d18a8f84ea7efed7a6a54477f9ae2678
Rafikov Timur <rafegg21@gmail.com>
2022-01-14 18:37:28 +0500

    #fix benefits#

--- components/Cos-benefits.vue
+++ components/Cos-benefits.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <h2 class="benefits-title title-2">Наши преимущества</h2>
+    <h2 class="title-2">Наши преимущества</h2>
     <div class="benefits-list">
       <div class="benefits-item" v-for="benefit in benefits">
         <div class="benefits-img">
@@ -53,7 +53,15 @@ export default {
 <style lang="scss" scoped>
 .container {
   width: auto;
-  padding: 12rem 34rem;
+  padding: 12rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.title-2 {
+  margin-bottom: 5.6rem;
+  text-align: center;
 }
 
 .icon {
@@ -63,12 +71,17 @@ export default {
 
 .benefits {
   &-list {
+    margin: 0 auto;
     display: flex;
     align-items: center;
   }
 
   &-item {
     max-width: 27rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
 
     &:not(:last-child) {
       margin-right: 2.7rem;
@@ -81,8 +94,14 @@ export default {
     justify-content: center;
     width: 8.8rem;
     height: 8.8rem;
+    margin-bottom: 1.6rem;
     border-radius: 50%;
     background: $pink-light;
+  }
+
+  &-title {
+    margin-bottom: .5rem;
+    font-size: 2.4rem;
   }
 }
 </style>
